34f7f43e7fd003e8757e7b2f2f127e36d113038f
Rasmus Kippasto <rasmus.kippasto@khk.ee>
2020-04-14 11:06:34 +0300

    PHP 7.0 paketi instaleerimis skript

---
+++ php_paigaldus.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PHP=$(dpkg-query -W -f='${Status}' php7.0 2>/dev/null | grep -c 'ok installed')
+
+if [ $PHP -eq 0 ]; then
+	echo "PHP versioon 7.0 paigaldamine..."
+	apt install php7.0 libapache2-mod-php7.0 php7.0-mysql -y
+	echo "PHP 7.0 instaleeritud"
+elif [ $PHP -eq 1 ]; then
+	echo "PHP on juba instaleeritud"
+	which php
+fi
