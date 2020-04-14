6951841e4bbf0a9934422937930fb9dca1a465a3
Rasmus Kippasto <rasmus.kippasto@khk.ee>
2020-04-14 11:34:09 +0300

    PHP 7.0 paigaldus

--- php_paigaldus.sh
+++ php_paigaldus.sh
@@ -4,7 +4,7 @@ PHP=$(dpkg-query -W -f='${Status}' php7.0 2>/dev/null | grep -c 'ok installed')
 
 if [ $PHP -eq 0 ]; then
 	echo "PHP versioon 7.0 paigaldamine..."
-	apt install php7.0 libapache2-mod-php7.0 php7.0-mysql -y
+	apt-get install php7.0 libapache2-mod-php7.0 php7.0-mysql -qq > /dev/null
 	echo "PHP 7.0 instaleeritud"
 elif [ $PHP -eq 1 ]; then
 	echo "PHP on juba instaleeritud"
