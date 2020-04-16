3dafece530136898315eb16a6aaccd9f3832c925
Rasmus Kippasto <rasmus.kippasto@khk.ee>
2020-04-16 09:24:35 +0300

    PHPmyadmin paigaldus

---
+++ pma_paigaldus.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PHPMA=$(dpkg-query -W -f='{$Status}' phpmyadmin 2>/dev/null | grep -c 'ok installed')
+
+if [ $PHPMA -eq 0 ]; then
+	echo "PHPmyadmin paigaldamine koos vajalike lisadega...."
+	apt-get install phpmyadmin -q
+	echo "PHPmyadmin on paigaltatud"
+elif [ $PHPMA -eq 1 ]; then
+	echo "PHPmyadmin on juba paigaltatud"
+fi
